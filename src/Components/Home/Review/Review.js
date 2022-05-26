5d3c35f572002da7f510d622777541f8f7bdc3e2
Swoad-11 <toufiq11swoad@gmail.com>
2022-05-26 13:08:17 +0600

    few customizaton

--- src/Components/Home/Review/Review.js
+++ src/Components/Home/Review/Review.js
@@ -8,9 +8,9 @@ const review = () => {
                 <div>
                     <h1 className="secondary-title text-4xl font-bold mb-8 text-center text-cyan-900">Customer Reviews</h1>
                 </div>
-                <div id="carouselExampleCaptions" class="carousel slide relative carousel-dark" data-bs-ride="carousel">
-                    <div class="carousel-inner relative w-full overflow-hidden">
-                        <div class="carousel-item active relative float-left w-full text-center">
+                <div id="carouselExampleCaptions" className="carousel slide relative carousel-dark" data-bs-ride="carousel">
+                    <div className="carousel-inner relative w-full overflow-hidden">
+                        <div className="carousel-item active relative float-left w-full text-center">
                             <p className="description text-cyan-700 text-base mb-4 italic">
                                 <svg
                                     aria-hidden="true"
@@ -29,7 +29,7 @@ const review = () => {
                                 </svg>
                                 They had the bike i wanted in stock and at a great price. Walked out the door with it same day - ez peezy!
                             </p>
-                            <div class="mt-12 mb-6 flex justify-center">
+                            <div className="mt-12 mb-6 flex justify-center">
                                 <ul className="flex justify-center">
                                     <li>
                                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
@@ -59,9 +59,9 @@ const review = () => {
 
                                 </ul>
                             </div>
-                            <p class="text-gray-500">- Anna Morian</p>
+                            <p className="text-gray-500">- Anna Morian</p>
                         </div>
-                        <div class="carousel-item relative float-left w-full text-center">
+                        <div className="carousel-item relative float-left w-full text-center">
                             <p className="description text-cyan-700 text-base mb-4 italic">
                                 <svg
                                     aria-hidden="true"
@@ -80,7 +80,7 @@ const review = () => {
                                 </svg>
                                 They had the bike i wanted in stock and at a great price. Walked out the door with it same day - ez peezy!
                             </p>
-                            <div class="mt-12 mb-6 flex justify-center">
+                            <div className="mt-12 mb-6 flex justify-center">
                                 <ul className="flex justify-center">
                                     <li>
                                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
@@ -110,9 +110,9 @@ const review = () => {
 
                                 </ul>
                             </div>
-                            <p class="text-gray-500">- Teresa May</p>
+                            <p className="text-gray-500">- Teresa May</p>
                         </div>
-                        <div class="carousel-item relative float-left w-full text-center">
+                        <div className="carousel-item relative float-left w-full text-center">
                             <p className="description text-cyan-700 text-base mb-4 italic">
                                 <svg
                                     aria-hidden="true"
@@ -131,7 +131,7 @@ const review = () => {
                                 </svg>
                                 They had the bike i wanted in stock and at a great price. Walked out the door with it same day - ez peezy!
                             </p>
-                            <div class="mt-12 mb-6 flex justify-center">
+                            <div className="mt-12 mb-6 flex justify-center">
                                 <ul className="flex justify-center">
                                     <li>
                                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="star" className="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
@@ -161,26 +161,26 @@ const review = () => {
 
                                 </ul>
                             </div>
-                            <p class="text-gray-500">- Kate Allise</p>
+                            <p className="text-gray-500">- Kate Allise</p>
                         </div>
                     </div>
                     <button
-                        class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
                         type="button"
                         data-bs-target="#carouselExampleCaptions"
                         data-bs-slide="prev"
                     >
-                        <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
+                        <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
                     </button>
                     <button
-                        class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
                         type="button"
                         data-bs-target="#carouselExampleCaptions"
                         data-bs-slide="next"
                     >
-                        <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
+                        <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
                     </button>
                 </div>
             </div>
